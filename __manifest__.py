c27ffab56968e0880791e638d382c0a204bc292d
Henry Navarro <henrymanuelnavarro@gmail.com>
2023-02-11 09:36:23 -0400

    First Commit

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Thesis Management",
+    'summary': "Allows the registration and management of Degree Thesis.",
+    'description': "Module developed to fulfill community service.",
+    'author': "Alejandra Turen, Fatima Ospina, Henry Guzman and Henry Navarro",
+    'category': 'Uncategorized',
+    'version': '1.0.0',
+    'license': 'LGPL-3',
+    'depends': ['base'],
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+}
